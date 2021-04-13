d21187a1e74f7c963903a6384d831b610d3c9fde
root <zidanegimiga@gmail.com>
2021-04-13 20:03:13 +0000

    Created header file. Added the three fundamental functions of the shel.

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <unistd.h>
+
+/* Function Prototypes */
+char *read_line(void);
+char **split_line(char *line);
+void loop(void);
+int execute(char **args);
+
+
+#endif /* SHELL_H */
