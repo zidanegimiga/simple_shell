a2ae8c8b599f0c7576693d79ddf3842b33ca52c9
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-22 03:40:41 +0300

    Updated the header file

--- shell.h
+++ shell.h
@@ -2,22 +2,33 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <wait.h>
 #include <unistd.h>
+#include <string.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
 
+/* Environ global variable */
+extern char **environ;
 /* Function Prototypes */
-char *read_line(void);
-char **split_line(char *line);
-void loop(void);
+char *readline(void);
+char **splitline(char *line);
 int execute(char **args);
-int _strlen(const char *str);
+char *get_path(char *command);
+char *_strcat(char *dest, char *src);
+char **tokenize_env(char *path);
+int _strcmp(char *s1, char *s2);
+void sigint_handler(int sig);
+int _putchar(char c);
+char *_getenv(char *var);
+void _printenv(void);
+int _strlen(char *s);
+int _strncmp(char *str1, char *str2, int n);
 
 
 #endif /* SHELL_H */
